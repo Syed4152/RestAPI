e4c2d5b62a6bb9364d25f5f29449d789f34e8f9a
Syed <pbuhmtamim@gmail.com>
2022-01-21 02:02:21 -0600

    PostAPI Added

--- RestAssuredAPITesting/src/test/java/restAssuredTests/testGet.java
+++ RestAssuredAPITesting/src/test/java/restAssuredTests/testGet.java
@@ -1,5 +1,9 @@
 package restAssuredTests;
 import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
@@ -10,9 +14,29 @@ public class testGet {
 	
 	public void getInfo() 
 	{
+ RestAssured.baseURI = "https://rahulshettyacademy.com";
+ 
+ given().queryParam("key", "qa123").header("Content-Type", "application/json;charset=UTF-8")
+ .body("{\r\n"
+ 		+ "  \"location\": {\r\n"
+ 		+ "    \"lat\": -38.383447,\r\n"
+ 		+ "    \"lng\": 33.427362\r\n"
+ 		+ "  },\r\n"
+ 		+ "  \"accuracy\": 50,\r\n"
+ 		+ "  \"name\": \"Frontline house\",\r\n"
+ 		+ "  \"phone_number\": \"(+91) 983 893 3937\",\r\n"
+ 		+ "  \"address\": \"29, side layout, cohen 09\",\r\n"
+ 		+ "  \"types\": [\r\n"
+ 		+ "    \"shoe park\",\r\n"
+ 		+ "    \"shop\"\r\n"
+ 		+ "  ],\r\n"
+ 		+ "  \"website\": \"http://google.com\",\r\n"
+ 		+ "  \"language\": \"French-IN\"\r\n"
+ 		+ "}\r\n"
+ 		+ "")
+ .when().post("maps/api/place/add/json")
+ .then().assertThat().statusCode(200);
 		
-		given()
-		.when().get("https://reqres.in/api/users?page=2").then().statusCode(200).assertThat().body("email", equalTo("michael.lawson@reqres.in"));
 		
 		
 	}
